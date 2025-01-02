fa765505941425421416fd0bd36e6c10e483ea7c
AlexGonRod <alexgonrod83@gmail.com>
2025-01-02 10:08:49 +0100

    add css variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,70 +7,68 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    extend: {
-      colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "rgb(255, 255, 255)",
-        foreground: "rgb(33,33,33)",
-        primary: {
-          DEFAULT: "rgb(33, 33, 33)",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "rgb(109, 18, 18)",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "rgb(255, 255, 255)",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      keyframes: {
-        "accordion-down": {
-          from: { height: '0' },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
-    },
-  },
+	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
+		colors: {
+			border: "rgb(var(--border))",
+			input: "rgb(var(--input))",
+			ring: "rgb(var(--ring))",
+			foreground: "rgb(var(--foreground))",
+			background: "rgb(var(--background))",
+			primary: {
+				DEFAULT: "rgb(var(--primary))",
+				foreground: "rgb(var(--primary-foreground))",
+			},
+			secondary: {
+				DEFAULT: "rgb(var(--secondary))",
+				foreground: "rgb(var(--secondary-foreground))",
+			},
+			destructive: {
+				DEFAULT: "rgb(var(--destructive))",
+				foreground: "rgb(var(--destructive-foreground))",
+			},
+			muted: {
+				DEFAULT: "rgb(var(--muted))",
+				foreground: "rgb(var(--muted-foreground))",
+			},
+			accent: {
+				DEFAULT: "rgb(var(--accent))",
+				foreground: "rgb(var(--accent-foreground))",
+			},
+			popover: {
+				DEFAULT: "rgb(var(--popover))",
+				foreground: "rgb(var(--popover-foreground))",
+			},
+			card: {
+				DEFAULT: "rgb(var(--card))",
+				foreground: "rgb(var(--card-foreground))",
+			},
+		},
+		borderRadius: {
+			lg: "var(--radius)",
+			md: "calc(var(--radius) - 2px)",
+			sm: "calc(var(--radius) - 4px)",
+		},
+		keyframes: {
+			"accordion-down": {
+				from: { height: '0' },
+				to: { height: "var(--radix-accordion-content-height)" },
+			},
+			"accordion-up": {
+				from: { height: "var(--radix-accordion-content-height)" },
+				to: { height: "0" },
+			},
+		},
+		animation: {
+			"accordion-down": "accordion-down 0.2s ease-out",
+			"accordion-up": "accordion-up 0.2s ease-out",
+		},
+	},
   plugins: [],
 } satisfies Config;
